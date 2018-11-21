0612fd5ac15004ab549b8e300c7a6111be2623cc
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-21 09:01:28 +0100

    exercice_php_P3_2

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+  function hello($name)
+  {
+    echo 'Bonjour ' . $name ;
+  }
+  hello('Marie');
+  ?>
+</body>
+</html>
